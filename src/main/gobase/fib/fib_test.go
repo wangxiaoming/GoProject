73d311ca1c529dfd237391e33727b21ab106cd39
wangxiaoming <wangminghit@126.com>
2021-02-19 23:44:06 +0800

    数字交换的方式 TestExchange

--- src/main/gobase/fib/fib_test.go
+++ src/main/gobase/fib/fib_test.go
@@ -17,3 +17,14 @@ func TestFibList(t *testing.T) {
 	//t.Log(b)
 
 }
+
+// 交换两个变量的值
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 2
+	// tmp := a
+	// a = b
+	// b = a
+	a, b = b, a
+	t.Log(a, b)
+}
