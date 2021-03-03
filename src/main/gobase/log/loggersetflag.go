16c5b01d17329a938be87229fa0149b89dbfa595
wangxiaoming <wangming19930331@hotmail.com>
2021-03-03 15:17:22 +0800

    log 工具包测试

---
+++ src/main/gobase/log/loggersetflag.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+	log.Println("这是一条很普通的日志。")
+}
---
+++ src/main/gobase/log/loogfile.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	logFile, err := os.OpenFile("D:/logs/CPCN/Payment/test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("open log file failed, err:", err)
+		return
+	}
+	log.SetOutput(logFile)
+	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+	log.Println("这是一条很普通的日志。")
+	log.SetPrefix("[小王子]")
+	log.Println("这是一条很普通的日志。")
+}
