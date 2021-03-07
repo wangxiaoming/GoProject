90d2c6a78888c1a82779ec65bb84036e52a0feda
wangxiaoming <wangminghit@126.com>
2021-03-07 14:52:18 +0800

    cancel by context

---
+++ src/main/gobase/channel/contest_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func isCanceled(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func TestCanceledByConetxt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	for i := 0; i < 5; i++ {
+		go func(i int, ctx context.Context) {
+			for {
+				if isCanceled(ctx) {
+					break
+				}
+			}
+			time.Sleep(time.Second * 1)
+		}(i, ctx)
+	}
+	cancel()
+}
